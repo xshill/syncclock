51d4a257a6e06f1302131ff4a6b3ad40a41ce248
Francis Labelle <stashingpixels@gmail.com>
2017-03-15 16:34:46 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+
+namespace SyncClock
+{
+    class Program
+    {
+        static bool IsW32Time(ServiceController service)
+        {
+            return service.ServiceName.Equals("W32Time", StringComparison.OrdinalIgnoreCase);
+        }
+
+        static void Main(string[] args)
+        {
+            var services = ServiceController.GetServices();
+            var w32time = services.FirstOrDefault(IsW32Time);
+            
+            if(w32time != null)
+            {
+                try
+                {
+                    var status = w32time.Status;
+                    if(status != ServiceControllerStatus.Running &&
+                       status != ServiceControllerStatus.StartPending &&
+                       status != ServiceControllerStatus.ContinuePending)
+                    {
+                        w32time.Start();
+                    }
+
+                    w32time.WaitForStatus(ServiceControllerStatus.Running, new TimeSpan(0, 0, 10));
+
+                    if(w32time.Status == ServiceControllerStatus.Running)
+                    {
+                        var psi = new ProcessStartInfo("w32tm", "/resync");
+                        psi.UseShellExecute = false;
+                        psi.CreateNoWindow = true;
+                        psi.RedirectStandardError = true;
+                        psi.RedirectStandardInput = true;
+                        psi.RedirectStandardOutput = true;
+                        Process.Start(psi);
+                    }
+                }
+                catch(Exception)
+                {
+                }
+            }
+        }
+    }
+}
